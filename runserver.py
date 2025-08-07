aac702256e01aefd0adc5913d5e2e6f49936f80b
danny2520 <danielsegun101@gmail.com>
2025-08-07 16:19:08 +0100

    initial commit

--- runserver.py
+++ runserver.py
@@ -1,13 +1,4 @@
-"""
-This script runs the ninestars_investor_website application using a development server.
-"""
-from os import environ
-from ninestars_investor_website import app
+from main import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run(host='0.0.0.0', port=5555)
